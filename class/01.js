a83896168943a7069db6f29aae2ed588793142eb
Abigail <pineverdi@gmail.com>
2022-06-14 14:09:09 -0600

    class

---
+++ class/01.js
@@ -0,0 +1,12 @@
+const aplicacion = new Vue({
+    //aca iniciaremos a trabajar con vue
+    //'#app' es el nombre del id, todo el contenido dentro de ese id sera gestionado con vue
+    el: '#app',
+    data:{
+       fondo: 'bg-warning',
+       color: false 
+    },
+    methods:{
+
+    }
+})
