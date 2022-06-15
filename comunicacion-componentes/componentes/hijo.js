c625438420d6c3007f526db0790ebb9a9e6355a3
Abigail <pineverdi@gmail.com>
2022-06-15 13:48:09 -0600

    comunicacion entre componentes x2

--- comunicacion-componentes/componentes/hijo.js
+++ comunicacion-componentes/componentes/hijo.js
@@ -3,8 +3,17 @@ template: //html
 `
 <div class="py-5 bg-success">
 <h4>Componente hijo: {{numero}}</h4>
-
+<h4>Nombre: {{nombreHijo}}</h4>
 </div>
 `,
-props:['numero']
+props:['numero'],
+    data(){
+        return{
+            nombreHijo: 'Uziel'
+        }
+    },
+    //enviamos el componente del hijo al padre
+    mounted(){
+        this.$emit('nombreHijo', this.nombreHijo);
+    }
 })
--- comunicacion-componentes/componentes/padre.js
+++ comunicacion-componentes/componentes/padre.js
@@ -4,12 +4,13 @@ template: //html
 <div class="p-5 bg-dark text-white">
 <h2>Padre: {{numeroPadre}}</h2>
 <button class="btn btn-danger" @click="numeroPadre++">+</button>
-<hijo :numero="numeroPadre"></hijo>
+<hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
 </div>
 `,
     data(){
         return{
-            numeroPadre:0
+            numeroPadre:0,
+            nombrePadre:''
         }
     }
 })
